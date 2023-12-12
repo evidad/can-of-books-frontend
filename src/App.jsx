a58b3c1a366ecdf674894d6c54d12b7488303003
Errol Vidad <errolvidad@gmail.com>
2023-12-12 23:42:46 +0000

    added carousel, title and description add, save button, verified working on local

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 // import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import BestBooks from './components/BestBooks.jsx';
-// import About from './components/About';
+import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 let SERVER = import.meta.env.VITE_SERVER;
@@ -22,17 +23,18 @@ function App() {
   }
 
   return (
-    <>
+    <> 
+      {/* this router is a component that wraps all the stuff up that means everything inside it are children and the children can receive props */}
       <Router>
         <Header />
         <button onClick={fetchBestBooks}>Get Some Books</button>
+        {/* routes and route are also components */}
         <Routes>
-          <Route
-            exact path="/books"
-            element={<BestBooks bestBooks={bestBooks} />}
-          >
-          </Route>
-          {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+          {/* // path and elements are props
+          // in this, it says the path is /books so render the BestBooks component */}  
+          <Route exact path="/books" element={<BestBooks />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exactpath="/" element={<Home />} />
         </Routes>
         <Footer />
       </Router>
